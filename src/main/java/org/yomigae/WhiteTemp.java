2d555b761f97aca432c521ace068ba497ceb29c8
Tracy Scott <tracy.scott@gmail.com>
2019-04-27 19:55:47 -0700

    Added intensity knob to WhiteTemp pattern.

--- src/main/java/org/yomigae/WhiteTemp.java
+++ src/main/java/org/yomigae/WhiteTemp.java
@@ -7,13 +7,17 @@ import heronarts.lx.color.LXColor;
 import heronarts.lx.model.LXPoint;
 import heronarts.lx.parameter.CompoundParameter;
 
+import java.awt.*;
+
 @LXCategory(LXCategory.TEST)
 public class WhiteTemp extends LXPattern {
   public final CompoundParameter tempKnob = new CompoundParameter("Kelvins", 1900f, 1900f, 7000f).setDescription("R");
+  public final CompoundParameter intensityKnob = new CompoundParameter("Intensity", 1.0f, 0.0f, 1.0f);
 
   public WhiteTemp(LX lx) {
     super(lx);
     addParameter(tempKnob);
+    addParameter(intensityKnob);
   }
 
   /**
@@ -24,8 +28,12 @@ public class WhiteTemp extends LXPattern {
     int[] rgb = new int[3];
     float k = tempKnob.getValuef();
     convertKToRGB(k, rgb);
+    // Convert RGB to HSB and scale by intensityKnob
+    float[] hsb = new float[3];
+    Color.RGBtoHSB(rgb[0], rgb[1], rgb[2], hsb);
+    hsb[2] = intensityKnob.getValuef() * hsb[2];
     for (LXPoint p : model.points) {
-      colors[p.index] = LXColor.rgb(rgb[0], rgb[1], rgb[2]);
+      colors[p.index] = LXColor.hsb(hsb[0] *360.0f, hsb[1] * 100.0f, hsb[2] * 100.0f);
     }
   }
 
